6e03f8297abeb05e55210e275bb9c41d26804484
Alisha Soriano <sorianoalisha@gmail.com>
2021-11-21 13:47:09 -0500

    Add functionality for burger menu to change to 'close' image and back upon clicking.

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 
 const navBar = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.burger-menu');
+const mobileMenuImage = document.querySelector('.mobile-menu-img');
 
 mobileMenu.addEventListener('click', () => {
   // console.log('I\'ve been clicked');
   navBar.classList.toggle('show');
+
+  if(navBar.classList.contains('show')) {
+    mobileMenuImage.src = 'images/icon-close.svg';
+  }
+  else {
+    mobileMenuImage.src = 'images/icon-hamburger.svg';
+  }
 });
